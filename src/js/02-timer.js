9e8b3a9da60b8a24720569ddadf563222cf63dcd
NickNedilko <overlord311089@gmail.com>
2022-10-26 12:02:21 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,6 +12,7 @@ const refs = {
 
 refs.startBtn.setAttribute('disabled', 'disabled');
 let userDate = null;
+let timerId = null;
 const currentDate = Date.now();
 
 const options = {
@@ -36,7 +37,7 @@ refs.startBtn.addEventListener('click', startTimer);
 
 function startTimer() {
   refs.startBtn.setAttribute('disabled', 'disabled');
-  let timerId = setInterval(() => {
+  timerId = setInterval(() => {
     const currentTime = Date.now();
     let deltaTime = userDate - currentTime;
     let { seconds, minutes, hours, days } = convertMs(deltaTime);
